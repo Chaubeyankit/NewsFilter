fc721fa94a4bb18c6b2b64cfb22f288f5170aed8
ChaubeyAnkit <chaubeya877@gmail.com>
2022-07-07 17:57:42 +0530

    NewsFilter

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export class About extends Component {
+  render() {
+    return (
+      <div className="conaiter text-center">I'am About page</div>
+    )
+  }
+}
+
+export default About
